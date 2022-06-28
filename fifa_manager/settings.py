f7479486661d3e5d10f3e0558765d32460105ff8
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-06-28 06:48:24 +0600

    started app

--- fifa_manager/settings.py
+++ fifa_manager/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'debug_toolbar',
+    'coreapi',
+    'drf_yasg',
+    'manager',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,9 @@ WSGI_APPLICATION = 'fifa_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': ''
     }
 }
 
