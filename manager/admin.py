1fa3d136371451966ce35e2637e55ec134bcb7ef
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-03 04:22:27 +0600

    added user admin without email

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from manager.models import (
+    Player,
+    User,
+)
+
+
+admin.site.register(Player)
+
+class UserAdmin(BaseUserAdmin):
+    """Define Admin for User with email and no username"""
+    fieldsets = (
+        (
+            None,
+            {'fields': ('email', 'password')},
+        ),
+        (
+            'Permissions',
+            {'fields': ('is_admin',)},
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
+        (
+            'Important dates',
+            {'fields': ('last_login', 'date_joined')},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2'),
+            },
+        ),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
