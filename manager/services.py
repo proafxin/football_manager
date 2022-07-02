15c859e07303668b9bc6fa8e0b40a4e933b59d3b
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-03 03:45:03 +0600

    Create services.py

---
+++ manager/services.py
@@ -0,0 +1,18 @@
+"""
+Define business logic here
+
+Views will not directly interact with Models.
+Views will call a service from this layer.
+This layer will call models.
+This helps implement the principle: make models as fat as necessary but not views. 
+"""
+
+
+
+def get_error(message, status_code):
+    error = {
+        'message': message,
+        'status_code': status_code,
+    }
+
+    return error
