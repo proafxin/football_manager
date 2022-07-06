d7450b23015320081558b9e2db925ab02fb518b2
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-06 14:47:01 +0600

    added minimum 95% coverage validation

--- manage.py
+++ manage.py
@@ -8,6 +8,18 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fifa_manager.settings')
     try:
+        command = sys.argv[1]
+    except IndexError:
+        command = "help"
+    running_tests = command == "test"
+    if running_tests:
+        from coverage import Coverage
+
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -16,6 +28,15 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    if running_tests:
+        cov.stop()
+        cov.save()
+        covered = cov.report()
+        percentage = 95
+        if covered < percentage:
+            print(f'Coverage < {percentage}')
+            sys.exit(1)
 
 
 if __name__ == '__main__':
