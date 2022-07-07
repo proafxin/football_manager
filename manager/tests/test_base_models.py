b191ce6b687c2d6cdfe3d2b25a9068a6c6281da3
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-07 16:20:17 +0600

    tested base models

--- manager/tests/test_base_models.py
+++ manager/tests/test_base_models.py
@@ -1,14 +1,15 @@
 """Define tests for Models"""
 
-# from django.conf import settings
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils.crypto import get_random_string
 
-# from manager.models import Player
+from manager.submodels.base_models import Country
 
 
 UserModel = get_user_model()
+MAX_LENGTH = settings.MAX_LENGTH
 
 class TestModels(TestCase):
     """Test all models"""
@@ -56,3 +57,15 @@ class TestModels(TestCase):
         self.assertEqual(user.email, email)
         self.assertEqual(user.is_staff, True)
         self.assertEqual(user.is_superuser, True)
+
+    def test_country(self):
+        """Test Country model"""
+        country_name = 'RandomCountry'
+        # pylint: disable=no-member
+        country = Country.objects.create(
+            name=country_name
+        )
+        # pylint: disable=no-member
+        self.assertEqual(Country.objects.count(), 1)
+        self.assertEqual(country.name, country_name)
+        self.assertEqual(str(country), country_name)
