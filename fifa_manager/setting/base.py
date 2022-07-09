94e9917fa90f4271acf6795df930ddbe4c0146a8
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-09 22:43:13 +0600

    Update base.py

--- fifa_manager/setting/base.py
+++ fifa_manager/setting/base.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from os import environ
+from os import (
+    environ,
+    path,
+)
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -186,6 +189,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
