4b84617b5523f19c5823ce6f43da6ef63c245a5f
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-07-12 17:27:51 +0600

    added manager serializer and validated attribute category serializer

--- manager/serializers.py
+++ manager/serializers.py
@@ -1,11 +1,14 @@
 """Define all Manager serializers here"""
 
+from django import conf
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework import serializers, status
 
-from manager import models
+from manager import models, services
 
 UserModel = get_user_model()
+ATTRIBUTES: list[str] = conf.settings.ATTRIBUTES
+CATEGORIES: list[str] = conf.settings.CATEGORIES
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -33,8 +36,35 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "email", "password")
 
 
+class ManagerSerializer(serializers.ModelSerializer):
+    """Serialize Manager fields"""
+
+    class Meta:
+        """Specify fields to serialize"""
+
+        model = models.Manager
+        fields = "__all__"
+
+
 class AttributeCategorySerializer(serializers.ModelSerializer):
     """Serialize AttributeCategory fields"""
+
+    def validate(self, attrs):
+        """Check attribute and category are from the predefined list"""
+        attribute = attrs.get("attribute")
+        category = attrs.get("category")
+        if attribute not in ATTRIBUTES:
+            error = services.get_error(
+                message=f"{attribute} not in ATTRIBUTES", status_code=status.HTTP_400_BAD_REQUEST
+            )
+            raise serializers.ValidationError(detail=error, code=status.HTTP_400_BAD_REQUEST)
+        if category not in CATEGORIES:
+            error = services.get_error(
+                message=f"{category} not in CATEGORIES", status_code=status.HTTP_400_BAD_REQUEST
+            )
+            raise serializers.ValidationError(detail=error, code=status.HTTP_400_BAD_REQUEST)
+
+        return super().validate(attrs)
 
     class Meta:
         """Specify fields to serialize"""
